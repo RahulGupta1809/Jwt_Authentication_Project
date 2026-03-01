84e0bbc53e84f772fff30ea86049b389ae42a199
Rahul Gupta <rahuldevit2000@gmail.com>
2026-03-01 09:22:47 +0530

    Update ApplicationExceptionHandler.java
    Change For Exception Class

--- src/main/java/com/example/employee/jpa/exceptions/advice/ApplicationExceptionHandler.java
+++ src/main/java/com/example/employee/jpa/exceptions/advice/ApplicationExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-//Exception Class
+//Exception Class of all Classes
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
 	
@@ -25,3 +25,4 @@ public class ApplicationExceptionHandler {
 
 
 }
+
