1a7eb47636cd9e64fbf6773ded412d37842a3a0e
Rahul Gupta <rahuldevit2000@gmail.com>
2026-08-02 17:06:32 +0530

    Refactor UserInfoController formatting and comments

--- src/main/java/com/example/employee/jpa/controller/UserInfoController.java
+++ src/main/java/com/example/employee/jpa/controller/UserInfoController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.employee.jpa.model.UserInfo;
 
 import com.example.employee.jpa.service.UserInfoService;
-
+//USerinfoController
 @RestController
 @RequestMapping("/api/addUser")
 public class UserInfoController {
